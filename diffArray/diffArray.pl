89b0674c1731b839e2683af59d905f247cc14ee2
Guo Xuan-Chen <mouse20273387@gmail.com>
2018-04-17 05:29:09 -0400

    upload file diffArray.pl

---
+++ diffArray/diffArray.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+=head
+    File    : perlDiffArray.pl
+    Time    : 2018/04/16 23:54
+    Author  : Guo,Xuan-Chen
+=cut
+
+# /* pragmas */ #
+use strict;
+use warnings;
+use feature qw(say switch);
+use Data::Dumper qw(Dumper);
+use Array::Diff;
+
+# /* create array */ #
+my @arr1 = qw(One Two Three);
+my @arr2 = qw(Four Five Six);
+
+# /* difference array  */ #
+my $diff = Array::Diff->diff(\@arr1, \@arr2);
+my @addArray = @{$diff->added};
+my @delArray = @{$diff->deleted};
+
+# /* print difference array */ #
+if($#addArray != -1){
+    foreach(@addArray){
+        say;
+    }
+}
+
+if($#delArray != -1){
+    foreach(@delArray){
+        say;
+    }
+}
